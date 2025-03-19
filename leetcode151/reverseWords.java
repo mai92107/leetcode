bb8ada87efc49cbb19ba549bb9d4e416eebc32e4
Rafa <rafamai@blockaction.tech>
2025-03-19 11:56:24 +0800

    [-] temporary command

--- leetcode151/reverseWords.java
+++ leetcode151/reverseWords.java
@@ -4,19 +4,19 @@
 
 
 
-class Solution {
-    public String reverseWords(String s) {
-        String[] arr = s.trim().split("\\s+");
-        StringBuilder word = new StringBuilder();
+// class Solution {
+//     public String reverseWords(String s) {
+//         String[] arr = s.trim().split("\\s+");
+//         StringBuilder word = new StringBuilder();
 
-        for (int idx = arr.length-1; idx >=0; idx--) {
-            word.append(arr[idx].trim());
-            if (idx == 0) {
-                continue;
-            }
-            word.append(" ");            
-        }
+//         for (int idx = arr.length-1; idx >=0; idx--) {
+//             word.append(arr[idx].trim());
+//             if (idx == 0) {
+//                 continue;
+//             }
+//             word.append(" ");            
+//         }
 
-        return word.toString();
-    }
-}
+//         return word.toString();
+//     }
+// }
